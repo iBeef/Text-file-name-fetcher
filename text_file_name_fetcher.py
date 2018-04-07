ed4fcc105896d4b36cbc43b6da1880d86a1f3438
iBeef <38163383+iBeef@users.noreply.github.com>
2018-04-07 23:06:12 +0100

    Update text_file_name_fetcher.py

--- text_file_name_fetcher.py
+++ text_file_name_fetcher.py
@@ -1,7 +1,8 @@
 #! python3
 
-'''Text_file_name_fetcher.py - Fetches the filenames of text files that need
-importing into a results spreadsheet and copies them to the clipboard.'''
+'''Text_file_name_fetcher.py - Fetches the filenames of text files that
+need importing into a results spreadsheet and copies them to the
+clipboard.'''
 
 # Created by iBeef - 06/04/18 - ibeef1990@gmail.com
 
@@ -9,12 +10,9 @@ import os
 import pyperclip
 
 def text_file_name_fetcher():
-
     text_files = []
-
     # Iterate through each file in the scripts current working directory.
     for filename in os.listdir(os.getcwd()):
-
         # Check to see if the file extension is 'txt'
         if filename.split('.')[1] == 'txt':
             text_files.append(filename)
